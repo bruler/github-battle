3615f50b6bce799539c7685fe8f4ba5a502e3aa9
daemonSlayer <kpgupta98@gmail.com>
2016-12-17 23:36:12 +0530

    second player fix

--- app/config/routes.js
+++ app/config/routes.js
@@ -8,12 +8,14 @@ var IndexRoute = ReactRouter.IndexRoute;
 
 var Main = require('../components/Main');
 var Home = require('../components/Home');
-
+var PromptContainer = require('../containers/PromptContainer');
 
 var routes = (
   <Router history={hashHistory}>
     <Route path='/' component={Main}>
-      <IndexRoute path='/home' component={Home} />
+      <IndexRoute component={Home} />
+      <Route path='/playerOne' header='Player One' component={PromptContainer} />
+      <Route path='/playerTwo/:playerOne' header='playerTwo' component={PromptContainer} />
     </Route>
   </Router>
 );
